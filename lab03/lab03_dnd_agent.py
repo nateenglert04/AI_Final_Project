cf8b60e9697133d448f95f6bc75fc5530aaf524c
Nathan <nke5100@psu.edu>
2025-01-31 16:45:59 -0500

    Completed code for the Dungeon Master and User

--- lab03/lab03_dnd_agent.py
+++ lab03/lab03_dnd_agent.py
@@ -6,10 +6,18 @@ from ollama import chat
 from util.llm_utils import pretty_stringify_chat, ollama_seed as seed
 
 # Add you code below
-sign_your_name = 'Pulin Agrawal'
-model = ''
-options = {}
-messages = []
+sign_your_name = 'Nathan Englert'
+model = 'llama3.2'
+options = {'temperature': 0.8, 'max_tokens': 150}
+messages = [
+  {'role': 'system', 'content': 'You are the Dungeon Master in a single player Dungeons and Dragons game. \
+                                You are responsible for creating the world and the story for the game. The world should be detailed and should be explained on a large scale so the user understands the scope of their adventure. \
+                                In the world, there should be a variety of interesting locations, characters, and items that the user can interact with. \
+                                You are also responsible for determining the outcome of the user\'s actions and the consequences of those actions. \
+                                You must always present the user\'s options in a consistent and concise listed, numerical format. \
+                                The user\'s inventory, skills, and health should not be displayed in the chat, but you should keep track of it.'},
+  {'role': 'user', 'content': 'Start the game.'}             
+]
 
 
 # But before here.
@@ -20,6 +28,17 @@ while True:
   response = chat(model=model, messages=messages, stream=False, options=options)
   # Add your code below
 
+  # Print the Dungeon Master's response
+  print(f'Dungeon Master: {response.message.content}')
+
+  # Add the Dungeon Master's response to the messages list
+  messages.append({'role': 'assistant', 'content': response.message.content})
+
+  # Get the user's input
+  user_input = input('You: ')
+
+  # Add the user's input to the messages list
+  messages.append({'role': 'user', 'content': user_input})
 
   # But before here.
   if messages[-1]['content'] == '/exit':
