7dd0673aed115bad2c2afcd21e8186825a847c74
Nathan <nke5100@psu.edu>
2025-02-07 18:38:34 -0500

    Created prompt and code necessary for 7/10 tests

--- lab04/lab04.py
+++ lab04/lab04.py
@@ -20,8 +20,23 @@ def run_console_chat(sign, **kwargs):
                 print('Ending match:', ending_match)
             break
 
-lab04_params = {}
+lab04_params = {
+    'template_file': 'lab04/lab04_trader_chat.json',
+    'sign': 'Nathan Englert',
+    'end_regex': r'TRADE(.*)DONE'
+}
 
 if __name__ ==  '__main__':
     # run lab04.py to test your template interactively
+
+    # Load the trader template
+    trader_template_file = 'lab04/lab04_trader_chat.json'
+
+    # Run a console chat with the trader template
+    run_console_chat(template_file=trader_template_file,
+                     inventory = '["rare gem", "gold coin", "silver coin"]',
+                     sign = 'Nathan Englert',
+                     end_regex = r'TRADE(.*)DONE'
+                    )  
+
     pass
